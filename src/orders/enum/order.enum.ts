4e07da65114b0eacb7210601d07c0bc81decd4a5
JuanJoseMarinVS <j2marinlujan@gmail.com>
2024-08-15 22:08:41 -0500

    feat: update project up to module 11

--- src/orders/enum/order.enum.ts
+++ src/orders/enum/order.enum.ts
@@ -2,10 +2,12 @@ export enum OrderStatus {
 	PENDING = 'PENDING',
 	COMPLETED = 'COMPLETED',
 	CANCELLED = 'CANCELLED',
+	PAID = 'PAID'
 }
 
 export const OrderStatusList = [
 	OrderStatus.PENDING,
 	OrderStatus.COMPLETED,
 	OrderStatus.CANCELLED,
+	OrderStatus.PAID
 ];
